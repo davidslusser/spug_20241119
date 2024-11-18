afac502b28d413e5d91c749efe0229b76a827f5f
David Slusser <david.slusser@sealstorage.io>
2024-11-17 21:28:38 -0800

    cli3

--- src/cli.py
+++ src/cli.py
@@ -1,13 +1,52 @@
 #!/usr/bin/python
 
+import argparse
+import datetime
+import logging
+import time
 import sys
+import traceback
+
+
+__doc__ = """this is a really great cli"""
+
+__version__ = "0.0.1"
+
+
+def get_opts():
+    """Return an argparse object."""
+    parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument("-d", "--verbose", default=logging.INFO, action="store_const", const=logging.DEBUG, help="enable debug logging")
+    parser.add_argument("-v", "--version", action="version", version=__version__, help="show version and exit")
+    parser.add_argument("-t", "--time", dest="time", action="store_true", help="include execution time")
+
+    args = parser.parse_args()
+    logging.basicConfig(level=args.verbose)
+    return args
+
+
+def do_the_thing():
+    logging.debug("starting the real work...")
+    logging.info("mission accomplished!")
 
 
 def main():
-    if len(sys.argv) > 1:
-        print(f"hello {sys.argv[1]}!")
-    else:
-        print("hello")
+    try:
+        opts = get_opts()
+        if opts.time:
+            start = datetime.datetime.now()
+        
+        # call function(s) to do work here
+        do_the_thing()
+
+        if opts.time:
+            end = datetime.datetime.now()
+            logging.info(f"script completed in: {end - start}")
+    except Exception as err:
+        logging.error(err)
+        if opts.verbose == 10:
+            traceback.print_exc()
+        return 255
 
 
 if __name__ == "__main__":
